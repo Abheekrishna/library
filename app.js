52520bb8805f3ef25eeb164f5d2e1b281b2a39de
Abheekrishna <abheekrishna17@gmail.com>
2023-06-30 20:29:08 +0530

    Added event to store data that user given.

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const addBtn = document.getElementById('newBook');
 const formsDiv = document.getElementById('forms');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('page');
+const readInput = document.getElementById('read');
+const submitBtn = document.getElementById('submit');
 
 
 let count = 1;
 
-addBtn.addEventListener('click', () => {
-    
-    if(count === 1) {
-        formsDiv.style.display = 'flex';
-        count = 2;
-    } else if(count === 2) {
-        formsDiv.style.display = 'none';
-        count = 1;
-    }
-})
+
 
 let myLibrary = [];
 
@@ -28,3 +24,24 @@ function Book(title, author, pages, read) {
     }
 }
 
+
+function addBookToLibrary() {
+    addBtn.addEventListener('click', () => {
+        if(count === 1) {
+            formsDiv.style.display = 'flex';
+            count = 2;
+        } else if(count === 2) {
+            formsDiv.style.display = 'none';
+            count = 1;
+        }
+    })
+    
+    submitBtn.addEventListener('click', () => {
+        console.log(titleInput.value);
+        console.log(authorInput.value);
+        console.log(pagesInput.value);
+        console.log(readInput.value);
+    })
+}
+
+addBookToLibrary();
